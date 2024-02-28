abfb7dcf636c29b9105f1b84ef69721c4584761b
angrybayblade <vptl185@gmail.com>
2024-02-28 21:16:59 +0530

    feat: custom render method for django

---
+++ ph7/django/__init__.py
@@ -0,0 +1,43 @@
+"""Django template engine."""
+
+import typing as t
+
+from django.http import HttpRequest, HttpResponse, StreamingHttpResponse
+from django.template.loader import get_template
+
+from ph7.django.engine import PH7Templates, Template
+
+
+def render(  # pylint: disable=too-many-arguments
+    request: HttpRequest,
+    template_name: str,
+    context: t.Optional[t.Dict] = None,
+    content_type: t.Optional[str] = None,
+    status: t.Optional[int] = None,
+    using: t.Optional[str] = None,
+    stream: bool = False,
+):
+    """Render a template with given settings."""
+    template = t.cast(Template, get_template(template_name, using=using))
+    content = template.render(
+        context=context,
+        request=request,
+        stream=stream,
+    )
+    if not stream:
+        return HttpResponse(
+            content=content,
+            content_type=content_type,
+            status=status,
+        )
+    return StreamingHttpResponse(
+        streaming_content=content,
+        content_type=content_type,
+        status=status,
+    )
+
+
+__all__ = (
+    "PH7Templates",
+    "render",
+)
